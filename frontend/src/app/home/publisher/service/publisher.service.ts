983ec6c05f122e3a5940c9a5890b02b260ec7c99
Matheus Silva <mathsilva.code@gmail.com>
2026-05-20 20:59:27 -0300

    feat: add PublisherService methods

--- frontend/src/app/home/publisher/service/publisher.service.ts
+++ frontend/src/app/home/publisher/service/publisher.service.ts
@@ -1,8 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PublisherRequest } from '../dto/publisher-request';
+import { Observable } from 'rxjs';
+import { PublisherResponse } from '../dto/publisher-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PublisherService {
   
+  private readonly API_URL = '/api/publishers'
+
+  constructor(private http: HttpClient){}
+
+  create(request: PublisherRequest): Observable<PublisherResponse>{
+    return this.http.post<PublisherResponse>(`${this.API_URL}`, {...request});
+  }
+
+  findAllByName(name: string): Observable<PublisherResponse[]>{
+    return this.http.get<PublisherResponse[]>(`${this.API_URL}`, { 
+      params: 
+        {name: name}
+    });
+  }
+
+  delete(id: number): Observable<void>{
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
+
 }
